6b1bc3694cd474924ed775040e72b3a4aab94285
kaczmarzvk <adam.kaczmarzyk@protonmail.com>
2025-04-01 00:41:18 +0200

    test live template

---
+++ src/main/kotlin/andrasferenczi/templater/FreezedFileTemplate.kt
@@ -0,0 +1,11 @@
+package andrasferenczi.templater
+
+import com.intellij.codeInsight.template.TemplateActionContext
+import com.intellij.codeInsight.template.TemplateContextType
+
+class FlutterContext private constructor() :
+    TemplateContextType("Flutter Freezed snippets") {
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        return templateActionContext.file.name.endsWith(".dart")
+    }
+}
